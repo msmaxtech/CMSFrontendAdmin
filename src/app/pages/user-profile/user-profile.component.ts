08915af73ca354a55d52e90662096d89d9412ed4
Prem Kumar <msmaxtech@gmail.com>
2018-08-11 15:00:35 +0530

    display client name

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClientService} from '../../@core/data/client.service';
 
 @Component({
   selector: 'ngx-user-profile',
   styleUrls: ['./user-profile.component.scss'],
   templateUrl: './user-profile.component.html',
 })
-export class UserProfileComponent {
-
+export class UserProfileComponent implements OnInit {
+  client: any;
+  constructor(private clientService: ClientService) {
+  }
   starRate = 2;
   heartRate = 4;
+
+  ngOnInit() {
+
+    this.clientService.getClientMaster().subscribe((clients: any) => {
+      this.client = clients;
+      console.log('clients..' + this.client.clientName);
+    });
+  }
 }
